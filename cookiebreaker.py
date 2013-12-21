1b7dcc952b77bb445782b6dbec24885cf625a012
Daniel Chen <dychen@caltech.edu>
2013-12-20 21:48:09 -0800

    Added buildings.

--- cookiebreaker.py
+++ cookiebreaker.py
@@ -19,11 +19,11 @@ def compress_large(arr):
 
 def get_info():
     try:
-        cookies = int(raw_input('Enter number of cookies: '))
+        cookies = int(raw_input('Enter number of cookies in bank: '))
         cookies_earned = cookies # Or else it detects that you're cheating
-        cookie_clicks = int(raw_input('Enter number of cookie clicks: '))
-        golden_clicks = int(raw_input('Enter number of golden cookie clicks: '))
-        handmade_cookies = int(raw_input('Enter number of handmade cookies: '))
+        cookie_clicks = 0
+        golden_clicks = 0
+        handmade_cookies = cookie_clicks
         missed_golden_clicks = 0 # What is this?
         background_type = 0      # And this?
         milk_type = 0            # And this?
@@ -42,13 +42,33 @@ def get_info():
     info_str = ';'.join(info_arr)
     return info_str
 
+def get_buildings():
+    try:
+        cursors = int(raw_input('Enter number of cursors: '))
+        grandmas = int(raw_input('Enter number of grandmas: '))
+        farms = int(raw_input('Enter number of farms: '))
+        factories = int(raw_input('Enter number of factories: '))
+        mines = int(raw_input('Enter number of mines: '))
+        shipments = int(raw_input('Enter number of shipments: '))
+        alchemy_labs = int(raw_input('Enter number of alchemy_labs: '))
+        portals = int(raw_input('Enter number of portals: '))
+        time_machines = int(raw_input('Enter number of time_machines: '))
+        antimatter_condensers = int(raw_input('Enter number of antimatter condensers: '))
+    except ValueError:
+        print "Could not read input."
+    buildings_arr = [cursors, grandmas, farms, factories, mines, shipments, alchemy_labs,
+                     portals, time_machines, antimatter_condensers]
+    buildings_arr = map(lambda x: ','.join([str(x), str(x), str(0), str(0)]), buildings_arr)
+    buildings_str = ';'.join(buildings_arr)
+    return buildings_str
+
 def break_cookies():
-    VERSION = '1.036' # Current version
+    VERSION = '1.0402' # Current version. Change as necessary
     BUFFER = ''
     START_DATE = datetime.datetime.now().strftime('%s000') # Now
     PREFS = '111111' # Default prefs
-    INFO = get_info() # The good stuff
-    BUILDINGS = '0,0,0,0;0,0,0,0;0,0,0,0;0,0,0,0;0,0,0,0;0,0,0,0;0,0,0,0;0,0,0,0;0,0,0,0;0,0,0,0;' # Idk what this is?
+    INFO = get_info() # Cookie info
+    BUILDINGS = get_buildings()
     UPGRADES = '2251799813685249;2251799813685249;2251799813685249;2251799813685249;524289' # I can guess what this is
     ACHIEVEMENTS = '2251799813685249;8796093022209' # Same with this
     vals = [VERSION, BUFFER, START_DATE, PREFS, INFO, BUILDINGS, UPGRADES, ACHIEVEMENTS]
